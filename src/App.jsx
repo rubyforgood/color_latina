f5f860a7c4665b5ad309e36d57e853615d928491
Hugo Amorim <hugo.amorim@ey.com>
2023-04-15 14:13:35 -0600

    tailwind and material ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from '@mui/material/Button';
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +21,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <Button variant="contained">Hello World</Button> 
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
