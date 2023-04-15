a7975b46e4dccebf21cc783add65605403ebac99
Hugo Amorim <hugo.amorim@ey.com>
2023-04-15 15:13:48 -0600

    fixed tailwind and material ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Button from '@mui/material/Button';
+import { Button, Container } from '@mui/material/Button';
 
 
 
@@ -11,27 +11,9 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button variant="contained">Hello World</Button> 
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Container maxWidth="sm">
+          <Button variant="contained">Hello World</Button>
+        </Container>
     </div>
   )
 }
